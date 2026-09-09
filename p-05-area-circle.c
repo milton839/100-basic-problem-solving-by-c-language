a700be4a6073d9212e8a755a53916ca71c5df67d
milton839 <miltonbiswas839@gmail.com>
2026-09-09 12:37:55 +0600

    Circle area calculate

---
+++ p-05-area-circle.c
@@ -0,0 +1,10 @@
+// Given the radius r of a circle, calculate and print its area.
+#include <stdio.h>
+int main()
+{
+    int r;
+    scanf("%d", &r);
+
+    printf("Area of circle is: %f\n", 3.1416 * r * r);
+    return 0;
+}
