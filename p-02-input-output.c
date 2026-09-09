6d4cf26c6757ff7ee7dbbba30146651e0bf6348a
milton839 <miltonbiswas839@gmail.com>
2026-09-09 12:02:02 +0600

    input output implement

---
+++ p-02-input-output.c
@@ -0,0 +1,13 @@
+// Given an integer n, print the value of n.
+
+#include <stdio.h>
+int main()
+{
+    int n;
+
+    printf("Enter a value: ");
+    scanf("%d", &n);
+
+    printf("Value is: %d\n", n);
+    return 0;
+}
