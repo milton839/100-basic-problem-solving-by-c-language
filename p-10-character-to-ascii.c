cfada1232336b824e25e6730161c0d83e23217ff
milton839 <miltonbiswas839@gmail.com>
2026-09-09 15:15:56 +0600

    Charater to ASCII convert

---
+++ p-10-character-to-ascii.c
@@ -0,0 +1,14 @@
+// Given a character ch, return its ASCII value.
+#include <stdio.h>
+int findASCII(char ch){
+    // int ascii = 
+    return ch;
+}
+int main()
+{
+    char ch;
+    scanf("%c", &ch);
+    int ascii = findASCII(ch);
+    printf("%d\n", ascii);
+    return 0;
+}
